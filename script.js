8c7bf7b6bc8015d09fbeb9629dbbd74a1f2e29dd
Alexandr <alexander@burlachenko>
2022-11-05 23:48:50 +0200

    adds slider functionality and make markup a bit  more beautiful/adds some comments/prevent potentional bugs before page load

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 let gameSpeed = 10;
+// let gameFrame = 0; // another metod, but with "ragged" bug when changing speed by range
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "./layers/layer-1.png";
@@ -16,61 +17,79 @@ backgroundLayer4.src = "./layers/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "./layers/layer-5.png";
 
-let x = 0;
-// let x2 = 2400;
-
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = 2400;
-    this.height = 700;
-    this.x2 = this.width;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
-  }
-  update() {
-    this.speed = gameSpeed * this.speedModifier;
-    if (this.x <= -this.width) {
-      this.x = 0;
-    } else this.x = Math.floor(this.x - this.speed);
-    // if (this.x <= -this.width) this.x = 0;
-  }
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x + 2400, this.y, this.width, this.height);
-  }
-}
-
-const layer1 = new Layer(backgroundLayer1, 0.2);
-const layer2 = new Layer(backgroundLayer2, 0.4);
-const layer3 = new Layer(backgroundLayer3, 0.6);
-const layer4 = new Layer(backgroundLayer4, 0.8);
-const layer5 = new Layer(backgroundLayer5, 1);
-
-const gameObjects = [layer1, layer2, layer3, layer4, layer5];
-
-function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-  // ctx.drawImage(backgroundLayer3, x, 0);
-  // ctx.drawImage(backgroundLayer3, x + 2400, 0);
-  // x < -2400 ? (x = 0) : (x -= gameSpeed);
-  gameObjects.forEach((object) => {
-    object.update();
-    object.draw();
+// all code should run when page is propperly loaded and available:
+window.addEventListener("load", function () {
+  const slider = document.getElementById("slider");
+  slider.value = gameSpeed;
+  const showGameSpeed = document.getElementById("showGameSpeed");
+  showGameSpeed.innerHTML = gameSpeed;
+  slider.addEventListener("change", function (e) {
+    console.log(e.target);
+    console.log(e.target.value);
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = gameSpeed;
   });
 
-  // ctx.drawImage(backgroundLayer4, x, 0);
-  // if (x < -2400) x = 2400 + x2 - gameSpeed;
-  // x -= gameSpeed;
+  let x = 0;
+  // let x2 = 2400;
 
-  // ctx.drawImage(backgroundLayer4, x2, 0);
-  // if (x2 < -2400) x2 = 2400 + x - gameSpeed;
-  // x2 -= gameSpeed;
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 700;
+      this.x2 = this.width;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      if (this.x <= -this.width) {
+        this.x = 0;
+      }
+      this.x = Math.floor(this.x - this.speed);
+      // this.x = (gameFrame * this.speed) % this.width; // see 7 line
+    }
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(
+        this.image,
+        this.x + this.width,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+  }
 
-  requestAnimationFrame(animate);
-}
+  const layer1 = new Layer(backgroundLayer1, 0.2);
+  const layer2 = new Layer(backgroundLayer2, 0.4);
+  const layer3 = new Layer(backgroundLayer3, 0.6);
+  const layer4 = new Layer(backgroundLayer4, 0.8);
+  const layer5 = new Layer(backgroundLayer5, 1);
 
-animate();
+  const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+  function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    gameObjects.forEach((object) => {
+      object.update();
+      object.draw();
+    });
+    // gameFrame--; // see 7 line
+
+    // // old more 'spagetti-code' method:
+    // ctx.drawImage(backgroundLayer4, x, 0);
+    // if (x < -2400) x = 2400 + x2 - gameSpeed;
+    // x -= gameSpeed;
+    // ctx.drawImage(backgroundLayer4, x2, 0);
+    // if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+    // x2 -= gameSpeed;
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
