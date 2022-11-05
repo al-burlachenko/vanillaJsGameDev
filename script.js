46a2cc601dccf735380fd268a9858400ed27cdfb
Alexandr <alexander@burlachenko>
2022-11-05 18:15:16 +0200

    adds basic canvas project setup: markup, css and js settings

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-// setting correct scaling:
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
+// setting correct scaling (by default it's 300x150px):
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
