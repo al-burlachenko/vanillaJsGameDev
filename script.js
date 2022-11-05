6ee19bcf926962f317d46419ef72057373d79993
Alexandr <alexander@burlachenko>
2022-11-05 18:23:59 +0200

    adds layers

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const ctx = canvas.getContext("2d");
 // setting correct scaling (by default it's 300x150px):
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "./layers/layer-1.png";
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = "./layers/layer-2.png";
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = "./layers/layer-3.png";
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = "./layers/layer-4.png";
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = "./layers/layer-5.png";
